9231deb46310e0b0cc8672233bb7656ddda4545a
Colin Ramsay <colinramsay@gmail.com>
2015-02-27 20:20:17 +0000

    Tidy

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,21 @@
-var gulp = require('gulp'),
+let gulp = require('gulp'),
     glp = require('gulp-load-plugins')(),
     del = require('del'),
     debug = require('gulp-debug'),
     path = require('path'),
     webpack = require('gulp-webpack-build');
 
+
 const SOURCE_DIR = 'src/';
 const BUILD_DIR = 'www';
 const CONFIG_FILENAME = 'webpack.config.js';
-
-gulp.task('assets', function() {
-  var assets = [
-    'src/images/**',
-    'src/index.html'
-  ];
-
-  return gulp.src(assets)
-    .pipe(glp.changed(BUILD_DIR))
-    .pipe(gulp.dest(BUILD_DIR))
-    .pipe(glp.size({title: 'assets'}));
-});
 
 
 gulp.task('webpack', [], function() {
     return gulp.src(CONFIG_FILENAME)
         .pipe(webpack.compile())
-        .pipe(webpack.format({
-            version: true,
-            timings: true
-        }))
-        .pipe(webpack.failAfter({
-            errors: true,
-            warnings: true
-        }))
+        .pipe(webpack.format({ version: true, timings: true }))
+        .pipe(webpack.failAfter({ errors: true, warnings: true }))
         .pipe(gulp.dest(BUILD_DIR));
 });
 
